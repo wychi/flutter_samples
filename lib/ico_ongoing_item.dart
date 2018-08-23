441bf9725927037583298f80ff7fcd1b6bb7c528
wychi <wenyuan.chi@ileopard.com>
2018-08-23 14:42:08 +0800

    layout widget #2

--- lib/ico_ongoing_item.dart
+++ lib/ico_ongoing_item.dart
@@ -59,113 +59,131 @@ class IcoOngoingItem extends StatelessWidget {
     );
   }
 
+  Widget _buildCard({BuildContext context, Widget child}) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10.0, right: 10.0, bottom: 10.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Color(0xFF1B224E),
+          borderRadius: BorderRadius.all(Radius.circular(6.0)),
+        ),
+        child: child,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Row(
-          children: <Widget>[
-            SizedBox(
-              width: 8.0,
-            ),
-            _forDesignVisibility(
-              child: Text("$idx", style: Styles.text_xsmall),
-            ),
-            SizedBox(
-              width: 13.5,
-            ),
-            buildRoundImage(IMAGE_URL),
-            SizedBox(
-              width: 12.0,
-            ),
-            Expanded(
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Flexible(
-                        child: _forDesignVisibility(
-                          child: Text(
-                            name,
-                            maxLines: 1,
-                            softWrap: false,
-                            overflow: TextOverflow.fade,
-                            style: Styles.text_large_bold
-                                .copyWith(color: Styles.cnm_white),
+    return _buildCard(
+      context: context,
+      child: Stack(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              SizedBox(
+                width: 8.0,
+              ),
+              _forDesignVisibility(
+                child: Text("$idx", style: Styles.text_xsmall),
+              ),
+              SizedBox(
+                width: 13.5,
+              ),
+              buildRoundImage(IMAGE_URL),
+              SizedBox(
+                width: 12.0,
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Flexible(
+                          child: _forDesignVisibility(
+                            child: Text(
+                              name,
+                              maxLines: 1,
+                              softWrap: false,
+                              overflow: TextOverflow.fade,
+                              style: Styles.text_large_bold
+                                  .copyWith(color: Styles.cnm_white),
+                            ),
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        width: 12.0,
-                      ),
-                      Container(
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          color: Styles.cnm_white_10pa,
-                          borderRadius: BorderRadius.all(Radius.circular(2.0)),
+                        SizedBox(
+                          width: 12.0,
                         ),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 3.0, horizontal: 4.0),
-                          child: Text(
-                            symbol,
-                            style: Styles.text_xxsmall
-                                .copyWith(color: Styles.cnm_white_60pa),
+                        Container(
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: Styles.cnm_white_10pa,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(2.0)),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 3.0, horizontal: 4.0),
+                            child: Text(
+                              symbol,
+                              style: Styles.text_xxsmall
+                                  .copyWith(color: Styles.cnm_white_60pa),
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  Text(category, style: Styles.text_xsmall),
-                ],
+                      ],
+                    ),
+                    Text(category, style: Styles.text_xsmall),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: 84.0,
+              )
+            ],
+          ),
+          Positioned(
+            right: 44.0,
+            bottom: 0.0,
+            child: SizedBox(
+              width: 40.0,
+              child: IconButton(
+                key: Key("add_alert"),
+                onPressed: () {},
+                icon: Icon(Icons.add_alert),
+                color: Styles.cnm_white_40pa,
               ),
             ),
-            SizedBox(
-              width: 84.0,
-            )
-          ],
-        ),
-        Positioned(
-          right: 44.0,
-          bottom: 0.0,
-          child: SizedBox(
-            width: 40.0,
-            child: IconButton(
-              key: Key("add_alert"),
-              onPressed: () {},
-              icon: Icon(Icons.add_alert),
-              color: Styles.cnm_white_40pa,
-            ),
           ),
-        ),
-        Positioned(
-          right: 4.0,
-          bottom: 0.0,
-          child: SizedBox(
-            width: 40.0,
-            child: IconButton(
-              key: Key("add_favorite"),
-              onPressed: () {},
-              icon: Icon(Icons.star_border),
-              color: Styles.cnm_white_40pa,
-            ),
-          ),
-        ),
-        Positioned(
-          top: 0.0,
-          right: 0.0,
-          child: Container(
-              width: 48.0,
-              height: 22.0,
-              decoration: BoxDecoration(
-                color: Styles.cnm_indigo_700_60pa,
-                borderRadius:
-                    BorderRadius.only(bottomLeft: Radius.circular(6.0)),
+          Positioned(
+            right: 4.0,
+            bottom: 0.0,
+            child: SizedBox(
+              width: 40.0,
+              child: IconButton(
+                key: Key("add_favorite"),
+                onPressed: () {},
+                icon: Icon(Icons.star_border),
+                color: Styles.cnm_white_40pa,
               ),
-              alignment: Alignment.center,
-              child: Text(score)),
-        )
-      ],
+            ),
+          ),
+          Positioned(
+            top: 0.0,
+            right: 0.0,
+            child: Container(
+                width: 48.0,
+                height: 22.0,
+                decoration: BoxDecoration(
+                  color: Styles.cnm_indigo_700_60pa,
+                  borderRadius:
+                      BorderRadius.only(bottomLeft: Radius.circular(6.0)),
+                ),
+                alignment: Alignment.center,
+                child: Text(score)),
+          )
+        ],
+      ),
     );
   }
 
