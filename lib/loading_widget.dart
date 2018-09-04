8d334559ca190e0547140c288e97f5bf4a7f9450
wychi <wenyuan.chi@ileopard.com>
2018-09-04 14:48:01 +0800

    add loading cursor

--- lib/loading_widget.dart
+++ lib/loading_widget.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class LoadingWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(child: Container(child: CircularProgressIndicator()));
   }
 }
